1a3b143d909d3a7482ecc1f6097a8ac8b3e05072
shendunjunshandiangou <72876106+shendunjunshandiangou@users.noreply.github.com>
2026-07-12 22:19:27 +0800

    在机型页输出结构化数据

--- 胶片相机选购指南/quartz/quartz.layout.ts
+++ 胶片相机选购指南/quartz/quartz.layout.ts
@@ -5,6 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [
+    Component.CameraStructuredData(),
     Component.Readability(),
     Component.ContentLayout(),
     Component.IconTuning(),
